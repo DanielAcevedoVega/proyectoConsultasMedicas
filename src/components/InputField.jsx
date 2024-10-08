61f56e2c2bfefd4ced19dfc3bb60863070a46168
DanielAcevedoVega <antonydany236@gmail.com>
2024-10-08 17:16:21 -0500

    feat: :sparkles: Se añadio la funcion para ver la contraseña
    Se agrego varios ternarios para validar si el icono esta visible o no y que al darle click cambie los parametros del input para poder ver la contraseña pasandola a tipo text

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
+
 const InputField = ({type, placeholder, icon})=>{
+    const[isPasswordShown, setIsPasswordShown] = useState
+    (false);
     return(
         <div className='input__wrapper'>
-          <input type={type} placeholder={placeholder} className='input__field' required/>
+          <input type={isPasswordShown ? 'text': type} placeholder={placeholder} className='input__field' required/>
           <i className="material-symbols-outlined">{icon}</i>
           {type === 'password' &&(
-            <i className="material-symbols-outlined eye-icon">visibility_off</i>
+            <i onClick={()=>setIsPasswordShown(prevState =>!prevState)} className="material-symbols-outlined eye-icon">{isPasswordShown ? 'visibility' : 'visibility_off' }</i>
           )}
         </div>
     )

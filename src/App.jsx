b5a40b6cc30b27ae585c3302670d5f4b0a8b70aa
DanielAcevedoVega <antonydany236@gmail.com>
2024-10-08 15:32:20 -0500

    feat: :art: Se hizo la estructura del inicio de sesion
    Se importo iconos y fuentes para utilizar en el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 
 const App = () => {
   return (
-  <div>App</div>
+  <div className='login__container'>
+    <h2 className="from__title">Inicio de Sesion</h2>
+
+    <form action="#" className='login__form'>
+      <div className='input__wrapper'>
+        <input type="text" placeholder='Usuario' className='input__field' required/>
+        <i className="material-symbols-outlined">person</i>
+      </div>
+
+      <div className='input__wrapper'>
+        <input type="password" placeholder='Contraseña' className='input__field' required/>
+        <i className="material-symbols-outlined">lock</i>
+      </div>
+      <a href="#" className="forgot__pass__link">Olvidaste la Contraseña?</a>
+
+      <button className='login__button'>Ingresar</button>
+    </form>
+    <p className='signup__text'>No tienes una cuenta? <a href="#">Registrate</a></p>
+  </div>
+
  )
 }
 

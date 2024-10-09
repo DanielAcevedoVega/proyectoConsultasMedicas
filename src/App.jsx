9de75827cfa71221c04ed45ef7f7349466b4ce82
DanielAcevedoVega <antonydany236@gmail.com>
2024-10-08 19:09:18 -0500

    feat: :art: Se le puso una animacion para cambiar de ingresar a registrar
    Se utilizo una funcion de estado para validar en que seccion estaba y cambiar la animacion dependiendo si estaba en ingresar a registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputField from './components/inputField';
 import SignUp from './components/SignUp';
 
 const App = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const handleSignUpClick = ()=>{
+    setIsSignIn(false);
+  };
+
+  const handleSignInClick = ()=>{
+    setIsSignIn(true);
+  }
   return (
-  <div className='login__container'>
-    <SignUp/>
+  <div className={`login__container ${isSignIn ? 'animated-signin' : 'animated-signup'}`}>
+    <SignUp handleSignInClick={handleSignInClick}/>
     <div className='container__form'>
       <form action="#" className='login__form'>
       <h2 className="from__title">Inicio de Sesion</h2>
@@ -16,13 +25,13 @@ const App = () => {
           <a href="#">Olvidaste la Contraseña?</a>
         </div>
         <button className='login__button' type='submit'>Ingresar</button>
-        <p className='signup__text'>No tienes una cuenta? <a href="#" className="signup__link">Registrate</a></p>
+        <p className='signup__text'>No tienes una cuenta? <a href="#" className="signup__link" onClick={handleSignUpClick}>Registrate</a></p>
       </form>
       
     </div>
   </div>
 
  )
-}
+};
 
 export default App
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,30 +1,30 @@
-const SignUp = ()=>{
-    return(
-    <div className='container__signup'>
-      <form action="#" className='login__form'>
-      <h2 className="from__title">Registro</h2>
+const SignUp = ({ handleSignInClick  }) => {
+    return (
+        <div className='container__signup'>
+            <form action="#" className='login__form'>
+                <h2 className="from__title">Registro</h2>
 
-      <div className='input__wrapper'>
-        <input type="text" placeholder='Usuario' className='input__field' required/>
-        <i className="material-symbols-outlined">person</i>
-      </div>
-      <div className='input__wrapper'>
-        <input type="email" placeholder='Usuario' className='input__field' required/>
-        <i className="material-symbols-outlined">email</i>
-      </div>
-      <div className='input__wrapper'>
-        <input type="password" placeholder='Contraseña' className='input__field' required/>
-        <i className="material-symbols-outlined">lock</i>
-      </div>
-      <div className='input__wrapper'>
-        <input type="password" placeholder='Confirmar Contraseña' className='input__field' required/>
-        <i className="material-symbols-outlined">lock</i>
-      </div>
+                <div className='input__wrapper'>
+                    <input type="text" placeholder='Usuario' className='input__field' required />
+                    <i className="material-symbols-outlined">person</i>
+                </div>
+                <div className='input__wrapper'>
+                    <input type="email" placeholder='Correo Electronico' className='input__field' required />
+                    <i className="material-symbols-outlined">email</i>
+                </div>
+                <div className='input__wrapper'>
+                    <input type="password" placeholder='Contraseña' className='input__field' required />
+                    <i className="material-symbols-outlined">lock</i>
+                </div>
+                <div className='input__wrapper'>
+                    <input type="password" placeholder='Confirmar Contraseña' className='input__field' required />
+                    <i className="material-symbols-outlined">lock</i>
+                </div>
 
-        <button className='login__button' type='submit'>Registar</button>
-        <p className='signin__text'>Ya tengo una cuenta <a href="#" className="signin__link">Ingresar</a></p>
-      </form>      
-    </div>
+                <button className='login__button' type='submit'>Registar</button>
+                <p className='signin__text'>Ya tengo una cuenta <a href="#" onClick={handleSignInClick}>Ingresar</a></p>
+            </form>
+        </div>
     )
 }
 export default SignUp

f4e0f59db7a69c30f420e86c541bb61f23cb7bc3
DanielAcevedoVega <antonydany236@gmail.com>
2024-10-15 20:37:41 -0500

    feat: :sparkles: Se realizo cambio para el inicio de sesion y cambiar a la siguiente pagina dinamicamente
    Se valida si los datos ingresados son correctos para enviar in useState y cambiar la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,70 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import InputField from './components/inputField';
 import SignUp from './components/SignUp';
-import login from './js/components/login';
+import MainPage from './MainPage';
 
 
 const App = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(()=>{
+    return localStorage.getItem('isLoggedIn') === 'true';
+  });
 
-  const handleSignUpClick = ()=>{
+  useEffect(()=>{
+    localStorage.setItem('isLoggedIn', isLoggedIn);
+  }, [isLoggedIn]);
+
+  const handleSignUpClick = () => {
     setIsSignIn(false);
   };
 
-  const handleSignInClick = ()=>{
+  const handleSignInClick = () => {
     setIsSignIn(true);
   }
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const emailV = e.target.elements.email_l.value;
+    const password = e.target.elements.pass_l.value;
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const email = users.find(user => user.email === emailV && user.password === password);
+    if (email) {
+      console.log('Inicio de sesión exitoso');
+      setIsLoggedIn(true);
+    } else {
+      console.log('Credenciales incorrectas');
+    }
+  };
+
+  const handleLogout = () => {
+    console.log('Funcion de cerrar sesion');
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+};
+
+  if (isLoggedIn) {
+    return <MainPage handleLogout={handleLogout} />
+  }
   return (
-  <div className={`login__container ${isSignIn ? 'animated-signin' : 'animated-signup'}`}>
-    <SignUp handleSignInClick={handleSignInClick}/>
-    <div className='container__form'>
-      <img src="public/logoAmesta.svg" alt="" />
-      <form action="#" className='login__form' onSubmit={login}>
-      <h2 className="from__title">Inicio de Sesion</h2>
-        <InputField id="email" type="email" name="email_l" placeholder='Correo Electronico' icon="email"/>
-        <InputField id="pass" type="password" name="pass_l" placeholder='Contraseña' icon="lock"/>
+    <div className={`login__container ${isSignIn ? 'animated-signin' : 'animated-signup'}`}>
+      <SignUp handleSignInClick={handleSignInClick} />
+      <div className='container__form'>
+        <img src="public/logoAmesta.svg" alt="" />
+        <form action="#" className='login__form' onSubmit={handleLogin}>
+          <h2 className="from__title">Inicio de Sesion</h2>
+          <InputField id="email" type="email" name="email_l" placeholder='Correo Electronico' icon="email" />
+          <InputField id="pass" type="password" name="pass_l" placeholder='Contraseña' icon="lock" />
 
-        <div className='forgot__pass__link'>
-          <a href="#">Olvidaste la Contraseña?</a>
-        </div>
-        <button className='login__button' type='submit'>Ingresar</button>
-        <p className='signup__text'>No tienes una cuenta? <a href="#" className="signup__link" onClick={handleSignUpClick}>Registrate</a></p>
-      </form>
-      
+          <div className='forgot__pass__link'>
+            <a href="#">Olvidaste la Contraseña?</a>
+          </div>
+          <button className='login__button' type='submit'>Ingresar</button>
+          <p className='signup__text'>No tienes una cuenta? <a href="#" className="signup__link" onClick={handleSignUpClick}>Registrate</a></p>
+        </form>
+
+      </div>
     </div>
-  </div>
 
- )
+  )
 };
 
 export default App
